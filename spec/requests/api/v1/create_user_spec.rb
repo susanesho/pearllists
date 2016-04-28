b4fe14609172dd70cf6057db09a97865a05efdea
andela-oesho <susan.esho@andela.com>
2016-04-29 00:27:00 +0100

    test(create users): test creating users
    test creating users in various scenarious
    
    [delivers #118610701]

---
+++ spec/requests/api/v1/create_user_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+RSpec.describe "Create Bucketlist", type: :request do
+  before(:all) do
+    @user = create(:user)
+  end
+
+  after(:all) do
+    User.destroy_all
+  end
+
+  describe "Post /users" do
+    context "valid params" do
+      it "creates new users" do
+        post(
+          "/api/v1/users/",
+          { name: "tayelolu",
+            email: "tayelolu@gmail.com",
+            password: "tayeloluejire345"
+          }
+        )
+        json_response = JSON.parse(response.body)
+
+        expect(response).to have_http_status(200)
+        expect(json_response["name"]).to eq "tayelolu"
+      end
+    end
+
+    context "invalid params" do
+      it "creates a user" do
+        post(
+          "/api/v1/users/",
+          { name: "tayelolu",
+            email: "",
+            password: "tayeloluejire345"
+          }
+        )
+        json_response = JSON.parse(response.body)
+
+        expect(response).to have_http_status(422)
+        expect(json_response["email"]).to eq ["can't be blank"]
+      end
+    end
+  end
+end
