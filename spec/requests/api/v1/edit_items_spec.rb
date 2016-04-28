336da3dc205fe00a8b6ab7c576238b4b17e57501
andela-oesho <susan.esho@andela.com>
2016-04-28 23:39:50 +0100

    test (edit items) edit items for bucketlist
    test editing scenarios for items which exist
    
    [delivers #118582959]

---
+++ spec/requests/api/v1/edit_items_spec.rb
@@ -0,0 +1,52 @@
+require "rails_helper"
+RSpec.describe "Create Bucketlist", type: :request do
+  before(:all) do
+    @user = create(:user)
+    @token = set_login(@user)
+  end
+
+    after(:all) do
+    User.destroy_all
+    Bucketlist.destroy_all
+  end
+
+  describe "put /bucketlists/:id/items/:id" do
+    context "when bucketlist items exists for user" do
+      it "edits a single bucketlist item" do
+        create_bucketlist(@user, @token, 1)
+        create_item(@user, @token, 1)
+        bucketlist = Bucketlist.last
+        item = Item.last
+
+        put(
+          "/api/v1/bucketlists/#{bucketlist.id}/items/#{item.id}",
+          { name: "buck" },
+          HTTP_AUTHORIZATION: @token
+        )
+
+        json_response = JSON.parse(response.body)
+        expect(json_response["item"]["name"]).to eq "buck"
+        expect(response).to have_http_status(201)
+      end
+
+      context "when item does not exists for user" do
+        it "cannot edits an item not created or found" do
+          create_bucketlist(@user, @token, 1)
+          create_item(@user, @token, 1)
+          bucketlist = Bucketlist.last
+          item = Item.last
+
+          put(
+            "/api/v1/bucketlists/2000/items/#{item.id}",
+            { name: "buck" },
+            HTTP_AUTHORIZATION: @token
+          )
+
+          json_response = JSON.parse(response.body)
+          expect(json_response["error"]).to eq "cannot update item"
+          expect(response).to have_http_status(403)
+        end
+      end
+    end
+  end
+end
