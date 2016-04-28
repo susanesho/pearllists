6a8ee943bcc1180d1adcf538a7a17dc7fea104de
andela-oesho <susan.esho@andela.com>
2016-04-28 18:06:19 +0100

    test(create items) test creating items for bucketlist
    test endpoints for posting items with expected scenarios
    
    [delivers #118576463]

---
+++ spec/requests/api/v1/create_items_spec.rb
@@ -0,0 +1,59 @@
+require "rails_helper"
+RSpec.describe "Create Bucketlist", type: :request do
+  before(:all) do
+    @user = create(:user)
+    @token = set_login(@user)
+  end
+
+  after(:all) do
+    User.destroy_all
+    Bucketlist.destroy_all
+  end
+
+  describe "Post /items" do
+    context "valid params" do
+      it "creates an item" do
+        create_bucketlist(@user, @token, 10)
+        bucketlist = Bucketlist.last
+
+        post(
+          "/api/v1/bucketlists/#{bucketlist.id}/items",
+          { name: "mynames" },
+          HTTP_AUTHORIZATION: @token
+        )
+        json_response = JSON.parse(response.body)
+
+        expect(response).to have_http_status(200)
+        expect(json_response["item"]["name"]).to eq "mynames"
+      end
+    end
+
+    context "when a user creates an item without a name" do
+      it "renders error" do
+        create_bucketlist(@user, @token, 1)
+        bucketlist = Bucketlist.last
+
+        post(
+          "/api/v1/bucketlists/#{bucketlist.id}/items",
+          { name: "" },
+          HTTP_AUTHORIZATION: @token
+        )
+        json_response = JSON.parse(response.body)
+
+        expect(response).to have_http_status(422)
+        expect(json_response["name"]).to eq ["can't be blank"]
+      end
+    end
+
+    context "when a bucketlist id does not exist or belong to user" do
+      it "renders error" do
+        create_bucketlist(@user, @token, 1)
+        post("/api/v1/bucketlists/2000/items", nil, HTTP_AUTHORIZATION: @token)
+        json_response = JSON.parse(response.body)
+
+        expect(response).to have_http_status(403)
+        expect(json_response["error"]).to eq "bucket item cannot be created"
+      end
+    end
+  end
+end
