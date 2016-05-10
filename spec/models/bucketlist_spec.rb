f683e2a37c835539b9e6b2cbaac7f4d7c670bdc8
andela-oesho <susan.esho@andela.com>
2016-05-10 18:42:25 +0100

    feat(test): create model test
    add model test to test instance method calls and validations
    
    [ delivers #119292465 ]

--- spec/models/bucketlist_spec.rb
+++ spec/models/bucketlist_spec.rb
@@ -1,4 +1,30 @@
 require "rails_helper"
 
 RSpec.describe Bucketlist, type: :model do
+  let(:bucketlist) { Bucketlist.new }
+
+  describe "instance method calls" do
+    it { expect(bucketlist).to respond_to(:name) }
+    it { expect(bucketlist).to respond_to(:id) }
+    it { expect(bucketlist).to respond_to(:user_id) }
+  end
+
+  describe "creating items" do
+    context "when name is present" do
+      it "creates bucketlist" do
+        name = "soyamilk"
+        bucketlist = Bucketlist.new(name: name)
+        expect(bucketlist).to be_valid
+      end
+    end
+
+     context "when name is not present" do
+      it "does not create bucketlist" do
+        name = ""
+        bucketlist = Bucketlist.new(name: name)
+        expect(bucketlist).to be_invalid
+        expect(bucketlist.errors[:name]).to include "can't be blank"
+      end
+    end
+  end
 end
