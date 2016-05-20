61db3d96be1dbe2e4fac43c294356c017b593778
andela-oesho <susan.esho@andela.com>
2016-05-20 12:18:09 +0100

    feat(centralize messages) add a class for messages
    add a class to handle all success and error messages

--- spec/requests/api/v1/edit_items_spec.rb
+++ spec/requests/api/v1/edit_items_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe "Edit items", type: :request do
           HTTP_AUTHORIZATION: set_login(item.bucketlist.user)
         )
 
-        expect(json_response["name"]).to eq ["can't be blank"]
+        expect(json_response["name"]).to be_truthy
         expect(response).to have_http_status(400)
       end
     end
